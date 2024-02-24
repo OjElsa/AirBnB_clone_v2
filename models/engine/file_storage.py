6679e0a337e346e0529266bf2eb85dd0246111b8
OjElsa <elsahojwang06@gmail.com>
2024-02-24 13:46:29 -0800

    Update to Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            nwdict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__:
+                    nwdict[key] = value
+            return nwdict
+        return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -20,8 +26,7 @@ class FileStorage:
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
+            for key, val in FileStorage.__objects.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
@@ -41,10 +46,16 @@ class FileStorage:
                     'Review': Review
                   }
         try:
-            temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it's inside"""
+        if obj is None:
+            key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
